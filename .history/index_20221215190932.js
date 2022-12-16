51cdede2200c51f070d031ff932a7a93267b9e3f
Julian Soto <sotoj760@gmail.com>
2022-12-15 19:40:38 -0700

    created a dice game
    used basic html css and javascript

---
+++ .history/index_20221215190932.js
@@ -0,0 +1,21 @@
+const randomNum = Math.floor(Math.random() * 6) + 1;
+const randomDice = "dice" + randomNum + ".png";
+
+const randomSrc = "images/" + randomDice;
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomSrc);
+
+const randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+const randomImgSrc2 = "images/dice" + randomNum2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
+
+if(randomNum > randomNum2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNum < randomNum2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else(randomNum = randomNum2) =>
+    document.querySelector("h1").innerHTML = "Draw!";
+    
